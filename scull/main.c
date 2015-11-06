a3f2fd4b6c46f5396d5cceba0e64824a6cc0f3d6
Marcin Kajzler <Marcin.Kajzler@Harman.com>
2015-11-06 14:32:18 +0100

    Empty stubs for file_operations
    Empty stubs for file_operations

--- scull/main.c
+++ scull/main.c
@@ -24,6 +24,47 @@ int scull_qset = SCULL_QSET;
 
 struct scull_dev *scull_devices;
 
+static int scull_open(struct inode *nodep, struct file *filp)
+{
+	return 0;
+}
+
+static ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
+{
+	return 0;
+}
+
+static loff_t  scull_llseek(struct file *filp, loff_t off, int whence)
+{
+	return 0;
+}
+
+static ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
+{
+	return 0;
+}
+
+static long scull_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	return 0;
+}
+
+static int scull_release(struct inode *inodep, struct file *filp)
+{
+	return 0;
+}
+
+static struct file_operations scull_fops =
+{
+	.owner   = THIS_MODULE,
+	.open    = scull_open,
+	.read    = scull_read,
+	.llseek  = scull_llseek,
+	.write   = scull_write,
+	.unlocked_ioctl   = scull_ioctl,
+	.release = scull_release,
+};
+
 static int __init scull_init_module(void)
 {
 	return 0;
--- scull/scull.h
+++ scull/scull.h
@@ -123,13 +123,12 @@ void    scull_access_cleanup(void);
 
 int     scull_trim(struct scull_dev *dev);
 
-ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
+static ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
                    loff_t *f_pos);
-ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
+static ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
                     loff_t *f_pos);
-loff_t  scull_llseek(struct file *filp, loff_t off, int whence);
-int     scull_ioctl(struct inode *inode, struct file *filp,
-                    unsigned int cmd, unsigned long arg);
+static loff_t  scull_llseek(struct file *filp, loff_t off, int whence);
+static long scull_ioctl(struct file *filep, unsigned int cmd, unsigned long arg);
 
 
 /*
